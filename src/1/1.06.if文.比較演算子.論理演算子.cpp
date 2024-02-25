11be296b82e4d9d92b7936f48e678d2d5c43af96
yossydev <codeyy021@gmail.com>
2024-02-25 23:59:53 +0900

    study

---
+++ src/1/1.06.if文.比較演算子.論理演算子.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// https://atcoder.jp/contests/apg4b/tasks/APG4b_g
+int main() {
+  // 入力は5だとしています。
+  int x;
+  cin >> x;
+ 
+  if (x < 10) {
+    cout << "xは10より小さい" << endl;
+  }
+ 
+  if (x >= 20) {
+    cout << "xは20以上" << endl;
+  }
+ 
+  if (x == 5) {
+    cout << "xは5" << endl;
+  }
+ 
+  if (x != 100) {
+    cout << "xは100ではない" << endl;
+  }
+ 
+  cout << "終了" << endl;
+}
