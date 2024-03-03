e79f7c1a3274c322342d4fdf4a7b29dba9eb3364
yossydev <codeyy021@gmail.com>
2024-03-03 18:57:02 +0900

    commit

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -6,15 +6,16 @@ OUT_DIR=./bin
 # 出力ディレクトリが存在しない場合は作成
 [ ! -d "$OUT_DIR" ] && mkdir -p "$OUT_DIR"
 
-# srcディレクトリとそのサブディレクトリ内の全ての.cppファイルに対して検索し、それぞれコンパイルする
+# srcディレクトリ内のすべての.cppファイルに対してコンパイルを実行
 find src -type f -name "*.cpp" | while read file; do
-    # ファイルのパスから基本名を取得（ディレクトリ部分を削除）
+    # ファイルのパスから基本名を取得（拡張子を除去）
     filename=$(basename "$file" .cpp)
-    # サブディレクトリを含めたパスから、すべてのスラッシュをアンダースコアに置換して、
-    # 一意の出力ファイル名を生成する
-    unique_name=$(echo "$file" | sed 's/src\/\?\|\.cpp//g' | sed 's/\//_/g')
-    # コンパイルコマンド
-    g++-13 -std=c++20 "$file" -o "$OUT_DIR/$unique_name"
+    # ファイルのパスからサブディレクトリを取得し、それをファイル名の一部とする
+    sub_dir=$(dirname "$file" | sed 's|src/||' | sed 's|/|_|g')
+    # 実行可能ファイルの名前を生成（サブディレクトリ名を含む）
+    exec_name="${sub_dir}_${filename}"
+    # コンパイルコマンドを実行
+    g++-13 -std=c++20 "$file" -o "$OUT_DIR/$exec_name"
 done
 
 echo "コンパイルが完了しました。"
